201fc0e7d6d03fb701889185480a33ade92d8b7e
ivan <ivanzigonipires.11@gmail.com>
2023-04-06 19:37:33 -0300

    feat: initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Teste1Module } from './teste1/teste1.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ExpenseModule } from './modules/expense/expense.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [Teste1Module, AuthModule, ExpenseModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
