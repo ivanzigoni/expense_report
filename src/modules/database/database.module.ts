c5463861348a616161a2547bcb8198380598b804
ivan <ivanzigonipires.11@gmail.com>
2023-04-06 20:46:23 -0300

    feat: database module

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
