b081d14b00ea746a5d99b364416ea5a597a6851b
ivan <ivanzigonipires.11@gmail.com>
2023-04-06 20:14:32 -0300

    feat: adds config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ExpenseModule } from './modules/expense/expense.module';
 import { DatabaseModule } from './modules/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, ExpenseModule, DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),AuthModule, ExpenseModule, DatabaseModule, ConfigModule],
   controllers: [],
   providers: [],
 })
