cc8429aec6ecaa4ca9c8a9ae9c575b667defb5c9
ivan <ivanzigonipires.11@gmail.com>
2023-04-06 19:43:09 -0300

    feat: adds auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { Teste1Module } from './teste1/teste1.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ExpenseModule } from './modules/expense/expense.module';
 import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [Teste1Module, AuthModule, ExpenseModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ExpenseModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
