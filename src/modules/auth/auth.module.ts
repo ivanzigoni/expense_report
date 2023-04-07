1065ca37b8ca4e3b760e7bd227ef37032ffcff23
ivan <ivanzigonipires.11@gmail.com>
2023-04-07 15:11:59 -0300

    feat: implements sign in route

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './service/auth.service';
 import { AuthController } from './controller/auth.controller';
 import { DatabaseModule } from '../database/database.module';
 import { UserModule } from '../database/user/user.module';
+import { JwtModule } from "@nestjs/jwt"
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [JwtModule.registerAsync({
+    global: true,
+    useFactory: (configService: ConfigService) => ({
+      global: true,
+      secret: configService.get("JWT_SECRET"),
+      signOptions: { expiresIn: "60s" }
+    }),
+    inject: [ConfigService],
+  }), UserModule],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Body, ClassSerializerInterceptor, Controller, Post, UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { UserService } from 'src/modules/database/user/service/user.service';
 import { CreateUserDto } from 'src/modules/database/user/user.dto';
+import { LoginCredentialsDto } from '../LoginCredentialsDto';
+import { AuthService } from '../service/auth.service';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('auth')
 export class AuthController {
   constructor(
-    private readonly userService: UserService 
+    private readonly userService: UserService,
+    private readonly authService: AuthService
   ) {}
 
   @Post("/user")
@@ -18,9 +21,9 @@ export class AuthController {
 
   @Post("/login")
   login(
-    @Body() loginCredentials: { email: string; password: string }
+    @Body(new ValidationPipe()) loginCredentials: LoginCredentialsDto
   ) {
-    
+    return this.authService.signIn(loginCredentials)
   }
 
 }
