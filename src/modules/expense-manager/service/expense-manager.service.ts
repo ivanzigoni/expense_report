aa94a66303c3940c9aaa53a30c8c031c71419bee
ivan <ivanzigonipires.11@gmail.com>
2023-04-08 12:00:05 -0300

    feat: mailer module working

--- src/modules/expense-manager/service/expense-manager.service.ts
+++ src/modules/expense-manager/service/expense-manager.service.ts
@@ -1,14 +1,52 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { ExpenseService } from 'src/modules/database/expense/service/expense.service';
+import { capitalizeFirstLetter } from 'src/modules/mailer/utils/string';
 import { SendEmailPayload } from '../interfaces/register-expense.dto';
 
 @Injectable()
 export class ExpenseManagerService {
   constructor(
     private readonly expenseService: ExpenseService,
+    private readonly mailerService: MailerService
   ) {}
 
   public async sendEmail(payload: SendEmailPayload) {
-    // email
+    // fila com redis?
+
+    const {
+      expense: {
+        description,
+        expenseDate,
+        expenseValue,
+        user: {
+          email,
+          firstName,
+          lastName,
+        }
+      }
+    } = payload;
+
+    const fullName = `${capitalizeFirstLetter(firstName)} ${capitalizeFirstLetter(lastName)}`
+    const date = expenseDate.toLocaleString("pt-br")
+    const value = expenseValue * 100 // reais
+
+    try {
+
+      return this.mailerService.sendMail({
+        to: email,
+        subject: `Despesa de ${fullName} cadastrada.`,
+        html: `
+          Sua despesa "${description}" efetuada no dia ${date} de valor ${value} reais foi cadastrada com sucesso!
+        `
+      })
+    
+    } catch (err) {
+
+      console.log(err)
+      console.log("err mail")
+
+    }
+
   }
 }
---
+++ src/modules/mailer/utils/string.ts
@@ -0,0 +1,17 @@
+function capitalizeFirstLetter(word: string) {
+  let output = "";
+
+  for (let i = 0; i < word.length; i += 1) {
+    if (i===0) {
+      output += word[i].toUpperCase()
+    } else {
+      output += word[i]
+    }
+  }
+
+  return output;
+}
+
+export {
+  capitalizeFirstLetter
+}
